70c721f63aa44b066cff3d753ea12c6d5fcbaa34
Kai <97007ken@gmail.com>
2021-03-12 00:36:10 +0800

    homework customers

---
+++ src/main/java/Customers.java
@@ -0,0 +1,23 @@
+public class Customers {
+    String id;
+    int cost;
+    int discount;
+    int total;
+
+    public Customers(String id, int cost) {
+        this.id = id;
+        this.cost = cost;
+    }
+
+    public void print() {
+        if (cost > 1000) {
+            discount = (int) (cost * 0.1);
+            total = cost - discount;
+            System.out.println(id + "\t" + cost + "\t" + discount + "\t" + total );
+        } else {
+            discount = 0 ;
+            total = cost-discount;
+            System.out.println(id + "\t" + cost + "\t" + discount + "\t" + total );
+        }
+    }
+}
